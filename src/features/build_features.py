057ed62aabaae58de1aa129d0cec01a5a5fed9a7
alexgiving <alexgiving@mail.ru>
2022-10-13 10:37:42 +0300

    t

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -10,6 +10,7 @@ from pyrsistent import optional
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('input_target_filepath', type=click.Path())  # how to make them optional
 #@click.argument('output_data_filepath', type=click.Path())
+#@click.argument('output_target_filepath', type=click.Path(optional=True))
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -17,7 +18,7 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-    # get features 
+    #zzzzzz 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
--- src/features/features.py
+++ src/features/features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def add_early_wakeup(df)-> pd.DataFrame:
     if df['wakeup_time'] < 6: df['early_wakeup'] = True
     return df
